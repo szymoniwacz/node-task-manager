670ffb508dbe62441fcdcafae5bb62a8a7b58e82
Szymon Iwacz <szymon@iwacz.pl>
2024-07-18 18:49:46 +0200

    Configured MongoDB connection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const config = require('./config');
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
+};
